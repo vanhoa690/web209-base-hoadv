6b9d78664fe139c7cf8da515775957be8e9472cb
vanhoa690 <vanhoa690@gmail.com>
2024-07-30 17:37:08 +0700

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useRoutes } from "react-router-dom";
+
 function App() {
+  const routes = useRoutes([]);
   return (
     <>
-      <h1 className="text-center">WEB209 + WEB502 - REACT + TYPESCRIPT - HOADV</h1>
+      <h1 className="text-center">
+        WEB209 + WEB502 - REACT + TYPESCRIPT - HOADV
+      </h1>
+      {routes}
     </>
   );
 }
